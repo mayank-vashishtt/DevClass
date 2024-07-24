80214ddd323641e93800128b00fb90eb75b494c7
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-07-25 03:23:21 +0530

    class 1

---
+++ main.js
@@ -0,0 +1,52 @@
+
+// // print("hello") -- python 
+// console.log("world")
+// console.log("world");
+
+
+// js start 
+// there is also one alternative of js in market 
+// typescript -- which is now more popular and more industry standard than js 
+
+// to know typescript we need to know js first
+
+// data type in js --- is this line correct ? the big NO -- why? 
+//  js is smarter it know which data type is this
+// majorly there are three datatypes in js 
+// 1. number
+// 2. string
+// 3. boolean
+
+function greet(){
+      console.log("hello ");
+
+    }
+
+
+// setTimeout(greet , 5 * 1000)
+
+// here 1sec == 1000 ms 
+
+
+
+// setInterval(greet, 2*1000)
+
+    
+
+
+// diff between setTImeout and setInterval 
+
+
+arr = [1,2,3,4,5] ; 
+// arr.push(0);
+// arr.pop();
+// arr.unshift(67797867987);
+// arr.shift();
+
+console.log(arr)
+
+
+// arr.push( arg )
+// arr.pop()
+// arr.unshift( arg  ) -- put in front
+// arr.shift() -- pop from front
