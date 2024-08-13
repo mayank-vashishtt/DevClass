ecb0fb54ef884d0b8c4f7b5ec9ee40e653d83d78
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-08-13 23:25:36 +0530

    theorgy js

---
+++ JS/basic.js
@@ -0,0 +1,54 @@
+// for the js , first you have to install nodejs in your system 
+console.log("hello world"); 
+// js can be run in browsers too 
+
+// js is an interpreted language 
+/*It seems like "Interpreted language" or "Interpreted lang." refers to a type of programming language that is
+ executed line by line by an interpreter, rather than being compiled into machine code beforehand. Examples of 
+ interpreted languages include Python, JavaScript, and Ruby.
+
+JS is an single threaded language
+A single-threaded language is a programming language that operates within a single thread of execution. 
+In simpler terms, when a program written in a single-threaded language runs, it executes one operation at a time, 
+in a sequential manner, from start to finish.
+
+
+Js is a dynamic language
+Dynamic languages are those where many of the features and checks are handled at runtime (when the program is executed) 
+rather than at compile time.
+ Variables in dynamic languages don't have a fixed type. You can assign a value of any type to a variable, 
+ and the type can change during the execution of the program.
+ Since types are determined at runtime, type-related errors are often caught only when the program is running
+ Dynamic languages offer great flexibility and are often easier and faster to write because you don't have to define types explicitly.
+ However, this flexibility can lead to runtime errors that are harder to catch.
+ Python, JavaScript, Ruby, PHP.
+
+
+ what is static language?
+ Static languages are those where most of the language features and type checks are performed at compile time, before the program is run.
+ Variables in static languages have a fixed type that is determined at compile time and cannot change.
+ Errors related to types are caught during the compilation process, reducing the chances of type-related runtime errors.
+  Static languages are generally considered safer because they enforce type constraints early, leading to more predictable and reliable code.
+  Java, C, C++, Rust, Go.
+
+
+
+  ECMAScript 
+  it is a scripting language in which js is based 
+ serve as the guideline on the rules for scripting language design ES6, ES5
+    ES6 is the latest version of ECMAScript, which was released in 2015. It introduced many new features and improvements to the language, 
+    such as let and const for variable declarations, arrow functions, classes, and modules.
+    ES5 is the previous version of ECMAScript, which was released in 2009. It is still widely used and supported by browsers and
+
+
+js
+it is a scripting lang that conforms to the ECMAScript specification 
+js even have additional feature -- setinterval , fs, 
+
+
+
+v8  - chrome open-source
+spidermonkey  -- firefox
+these are compiler which compile js code and should be changeed acc to latest ECMAScript updates 
+
+*/
